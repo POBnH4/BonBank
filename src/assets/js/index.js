d276c94061b04b2eb37802e14f5acf38e706c7b4
POBnH4 <bonchp4@yahoo.com>
2020-02-26 15:32:42 +0000

    frond-end index page done

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,9 +1,9 @@
+// Get the modal
+var modal = document.getElementById('id01');
 
-function openForm() {
-  document.getElementById("myForm").style.display = "block";
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
 }
-
-function closeForm() {
-  document.getElementById("myForm").style.display = "none";
-}
-
