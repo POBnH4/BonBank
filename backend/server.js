a716a40eeb07bec8b0020f1afde874bf013e6702
POBnH4 <bonchp4@yahoo.com>
2020-03-08 17:03:45 +0000

    backend setup

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,58 @@
 //Install express server
+
 const express = require('express');
 const path = require('path');
 
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/<name-of-app>'));
+http.createServer(function (request, response){
+  response.writeHead(200, {'Content-type': 'text/plain'});
+}).listen(8080);
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
+res.sendFile(path.join(__dirname + '../src/index.html'));
 });
 
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+const http = require('http');
+const express = require('express');
+const session = require('express-session'); //npm install express-session
+const bodyParser = require('body-parser'); //npm install body-parser
+//const MONGO_CLIENT = require('mongodb').MongoClient;
+const url = "mongodb://localhost:27017/";
+var app = express();
+
+const hostname = '3.134.92.177';
+//const hostname = '127.0.0.1';
+//const hostname = '18.216.27.105';
+
+console.log(`Server running at http://${hostname}:${port}/`);
+
+// Node.js body parsing middleware
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+var db;
+
+
+//app.use(session({secret: 'example'}));
+
+//////////////- - - - - -MONGO- - - - - -//////////////////////////
+//Connection to MongoDB - sets the variable db as our database
+//MongoClient.connect(url, function(err, database) {
+//    if (err) throw err;
+//    db = database;
+//    app.listen(8080);
+//    console.log('Mongo listening on 8080');
+//});
+
+///////////////- - - - - -END OF MONGO- - - - - -////////////////////////
+
+//app.use(express.static("public"));
+
+app.get('/*', function(req,res) {
+    res.render('pages/index');
+});
+
+///////////////////////////////////////////////////////////////////
