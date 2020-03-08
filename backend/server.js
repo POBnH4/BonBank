f8566e4f761f18476163feea8c8419be6029bc52
POBnH4 <bonchp4@yahoo.com>
2020-03-08 20:12:59 +0000

    added login, logout, show profile requests ( not finished)

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const express = require('express');
 const session = require('express-session'); //npm install express-session
 const bodyParser = require('body-parser'); //npm install body-parser
 const MongoClient = require('mongodb').MongoClient;
-const url = "mongodb://localhost:27017/";
+const url = "mongodb://localhost:27017/"; // localhost 8080 or 8090?
 const path = require('path');
 
 const app = express();
@@ -40,6 +40,49 @@ MongoClient.connect(url, function(err, database) {
     db = database;
     app.listen(8090);
     console.log('Mongo listening on 8090');
+});
+
+app.get('/profile', function(req,res) {
+    var uname = req.session.username;
+    if (req.session.loggedin) {
+        db.collection('users').findOne({"username":uname}, function(err,result) {
+            if (err) throw err;
+            res.render('pages/profile', {
+                "user": result
+            });
+        });
+    }
+    else {
+        res.redirect('/');
+    }
+});
+
+app.get('/logout', function(req,res) {
+    req.session.loggedin = false;
+    req.session.destroy();
+    res.redirect('/');
+});
+
+app.post('/login', function(req,res){
+    console.log(JSON.stringify(req.body));
+    var username = req.body.username;
+    var pword = req.body.password;
+    db.collection('users').findOne({"username":username}, function(err,result) {
+        if (err) throw err;
+        if (!result) {
+            res.redirect('/'); //TODO add POP up not found?
+            return;
+        }
+        if (result.password == pword) {
+            req.session.loggedin = true;
+            req.session.username = result.username;
+            res.redirect('/profile');
+        }
+        else {
+            console.log("Incorrect password!");
+            res.redirect('/');
+        }
+    });
 });
 
 ///////////////- - - - - -END OF MONGO- - - - - -////////////////////////
